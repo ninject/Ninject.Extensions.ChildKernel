43a440b19ca157c33bee4309755180528d638e18
remogloor <arvreICsGCwSu53ClGma>
2010-08-10 16:56:29 +0200

    Added build scripts

---
+++ src/Ninject.Extensions.ChildKernel.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Ninject.Extensions.ChildKernel.Test")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("63fdf296-9e16-4bed-8052-a3a71cdbd7a5")]
---
+++ src/Ninject.Extensions.ChildKernel/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Ninject.Extensions.ChildKernel")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("39527a43-4711-438a-b3a8-63dc45fe45ee")]
